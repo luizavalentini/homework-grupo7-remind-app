cbbda069e943541a0314f3fb2a1d41a0d75b1194
Kevin Aryel <kevinaryel.dev@gmail.com>
2022-10-21 21:52:17 -0300

    feat: Funcionalidade de addLembrete

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,23 @@
-let lembretes = [["Titulo do lembrete","out", 2022, "10:30"]]
+let lembretes = [["titulo", "mês", "ano", "hora"]];
 
+document.querySelector("form").addEventListener("submit", function (event) {
+  event.preventDefault();
+  addLembrete(event);
+});
 
+function manipularModal() {
+  modal = document.querySelector(".modal-container");
+  modal.classList.toggle("hidden");
+}
 
-
-
-function manipularModal(){
-    modal = document.querySelector('.modal-container')
-    modal.classList.toggle('hidden')
+function addLembrete(e) {
+  let formData = new FormData(e.target).entries();
+  let lembreteObj = Object.fromEntries(formData);
+  let lembreteArray = [];
+  lembreteArray.push(lembreteObj.descricao);
+  lembreteArray.push(lembreteObj.mes);
+  lembreteArray.push(lembreteObj.ano);
+  lembreteArray.push(lembreteObj.hora);
+  lembretes.push(lembreteArray);
+  manipularModal();
 }
