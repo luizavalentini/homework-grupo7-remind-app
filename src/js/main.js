b3b5fae6108e80e4fcce9e56ca45ca88e7fdae2a
Kevin Aryel <kevinaryel.dev@gmail.com>
2022-10-22 01:26:07 -0300

    feat: mudarMês agora é mudarData! O ano consegue ser manipulado pelas setas script organizado e comentado

--- src/js/main.js
+++ src/js/main.js
@@ -1,43 +1,71 @@
+// Inicialização de variáveis
+
 let lembretes = [
     ["Titulo do lembrete","out", 2022, "10:30"], 
     ["Ir na facul","nov", 2022, "10:30"], 
     ["fazer compras","out", 2022, "10:30"],
 ]
-document.querySelector("form").addEventListener("submit", function (event) {
-  event.preventDefault();
-  addLembrete(event);
-});
 
 let meses = ['jan','fev','mar','abr','mai','jun','jul','ago','set','out','nov','dez']
+
 let mesAtual = 10;
 let anoAtual = 2022;
 
-function mudarMesAvançar(){
-    let show = document.querySelector('.mostra');
-    show.innerHTML = '';
-    if(mesAtual<meses.length){
-        mesAtual++;
-        mostrarLembretes();
-   }
-    let mes = meses[mesAtual-1]
-    show.innerHTML += `<p>${mes}, ${2022}</p>`
+//Mostrar o mês e os lembretes dele quando a página carregar
+
+window.addEventListener("load",()=>{
+  mostrarData();
+  mostrarLembretes();
+})
+
+// Manipular mês/Ano
+
+function mostrarData(){
+  let show = document.querySelector('.mostra');
+  show.innerHTML = '';
+  let mes = meses[mesAtual-1]
+  show.innerHTML += `<p>${mes}, ${anoAtual}</p>`
 }
 
-function mudarMesVoltar(){
-    let show = document.querySelector('.mostra');
-    show.innerHTML = '';
-    if(mesAtual > 1){
-      mesAtual--;
-      mostrarLembretes();
-    }
-    let mes = meses[mesAtual-1]
-    show.innerHTML += `<p>${mes}, ${2022}</p>`
+function mudarDataAvançar(){
+  if(mesAtual<meses.length){
+    mesAtual++;
+    mostrarLembretes();
+    mostrarData();
+  } else {
+    mesAtual = 1
+    anoAtual ++
+    mostrarLembretes();
+    mostrarData();
+  }
 }
+
+function mudarDataVoltar(){
+  if(mesAtual > 1){
+    mesAtual--;
+    mostrarLembretes();
+    mostrarData();
+  } else {
+      anoAtual --
+      mesAtual = 12
+      mostrarLembretes();
+      mostrarData();
+  }
+}
+
+//Abrir / fechar modal
 
 function manipularModal() {
   modal = document.querySelector(".modal-container");
   modal.classList.toggle("hidden");
 }
+
+//Adicionar Lembretes
+
+document.querySelector("form").addEventListener("submit", function (event) {
+  event.preventDefault();
+  addLembrete(event);
+});
 
 function addLembrete(e) {
   let formData = new FormData(e.target).entries();
@@ -52,26 +80,21 @@ function addLembrete(e) {
   mostrarLembretes();
 }
 
-function manipularModal(){
-    modal = document.querySelector('.modal-container')
-    modal.classList.toggle('hidden')
-}
+//Mostrar Lembretes na tela
 
 function mostrarLembretes(){
   let show = document.querySelector('.tasks');
   show.innerHTML = '';
-  if(lembretes.length > 0){
-    let lembretesFiltrados = lembretes.filter((lembrete) => lembrete[1] == meses[mesAtual-1])   
-    if (lembretesFiltrados.length > 0 ){
-      lembretesFiltrados.forEach(el =>
-        show.innerHTML += `<div class="task-container task-container-item"><p>${el[0]}</p> <p>${el[el.length-1]}</p></div>`
-      )
+  let lembretesFiltrados = lembretes
+  .filter((lembrete) => lembrete[1] == meses[mesAtual-1])   
+  if (lembretesFiltrados.length > 0 ){
+    lembretesFiltrados
+    .forEach(el =>{
+      show.innerHTML += `<div class="task-container task-container-item">
+      <p>${el[0]}</p> <p>${el[el.length-1]}</p>
+      </div>`
+    })
     }else{
       show.innerHTML = `<div class="task-container">Nenhum lembrete cadastrado ainda</p></div>`
   }
- } else{
-    show.innerHTML = `<div class="task-container">Nenhum lembrete cadastrado ainda</p></div>`
- }
 }
-
-mostrarLembretes();
