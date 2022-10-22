b723ea9107ee95128a432a65d7f5becda1a99d9e
Kevin Aryel <kevinaryel.dev@gmail.com>
2022-10-22 14:08:18 -0300

    fix: removendo typecheck do ano.

--- src/js/main.js
+++ src/js/main.js
@@ -88,7 +88,7 @@ function mostrarLembretes(){
   let show = document.querySelector('.tasks');
   show.innerHTML = '';
   let lembretesFiltrados = lembretes
-  .filter((lembrete) => lembrete[1] === meses[mesAtual-1] && lembrete[2] === anoAtual )   
+  .filter((lembrete) => lembrete[1] === meses[mesAtual-1] && lembrete[2] == anoAtual )   
   if (lembretesFiltrados.length > 0 ){
     lembretesFiltrados
     .forEach(el =>{
