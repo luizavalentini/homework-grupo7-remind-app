b687e981a66becf5b9aae33c2ec03809e4023b51
Alain <alaindelonm@gmail.com>
2022-10-21 21:44:47 -0300

    feat: add mostrar todos os lembretes

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 let lembretes = [["Titulo do lembrete","out", 2022, "10:30"]]
-
+let show = document.querySelector('.tasks');
 
 
 
@@ -7,4 +7,12 @@ let lembretes = [["Titulo do lembrete","out", 2022, "10:30"]]
 function manipularModal(){
     modal = document.querySelector('.modal-container')
     modal.classList.toggle('hidden')
+}
+
+if(lembretes.length > 0) {
+   lembretes.map((lembrete) =>{ 
+        show.innerHTML += `<div class="task-container">${lembrete[0]} ${lembrete[lembrete.length-1]}</p></div>`
+    })
+}else{
+    show.innerHTML += `<div class="task-container">Nenhum lembrete cadastrado ainda</p></div>`
 }
